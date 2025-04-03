ce6ab8ba534d115428f04c12e256703c98dfcf55
Manish Singh <manishsingh021092@gmail.com>
2025-04-04 02:21:15 +0530

    feat(header): Add header

---
+++ src/app/components/header/header.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the correct title', () => {
+    const titleElement = fixture.nativeElement.querySelector('h1');
+    expect(titleElement.textContent).toContain('Quiz Notified');
+  });
+});
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+  <header class="bg-[#3929a0] text-white py-4 mb-8">
+    <div class="container">
+        <h1 class="text-2xl font-bold text-center">Quiz Notified</h1>
+    </div>
+  </header>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
